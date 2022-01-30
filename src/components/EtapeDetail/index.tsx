fc0d156a21a5c6af02080a65cd08923f03ade7c6
xseignard <xavier.seignard@gmail.com>
2022-01-30 01:35:50 +0100

    enh; custom scrollbar

--- src/components/EtapeDetail/index.tsx
+++ src/components/EtapeDetail/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 import { AnimatePresence, motion } from 'framer-motion';
 import ReactPlayer from 'react-player';
+import SimpleBar from 'simplebar-react';
 
 import { EtapeRecord } from '~/generated/sdk';
 import { useDateRangeCopy } from '~/utils/useDateRangeCopy';
+
+import 'simplebar/dist/simplebar.min.css';
 
 import Carousel from '../Carousel';
 
@@ -25,7 +28,7 @@ const EtapeDetail = ({
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className="fixed inset-6 bottom-12 md:inset-12 z-30 px-6 md:px-8 py-8 flex flex-col items-center"
+          className="fixed z-50 inset-6 bottom-12 md:inset-12 px-6 md:px-8 py-8 flex flex-col items-center"
           style={{ backgroundColor: color }}
         >
           <button
@@ -38,49 +41,53 @@ const EtapeDetail = ({
           <span className="text-xl md:text-3xl font-plex mb-6 md:mb-16">
             {etape.lieu} ~ {dateRangeCopy}
           </span>
-          <div className="flex flex-col md:flex-row flex-1 w-full overflow-x-hidden overflow-y-auto">
-            <div className="md:flex-1 max-w-screen-md mb-4 md:mb-0">
-              {etape.video && (
-                <div className="relative" style={{ paddingTop: '56.25%' }}>
-                  <ReactPlayer
-                    url={etape.video.url!}
-                    controls
-                    width="100%"
-                    height="100%"
-                    className="absolute inset-0 w-full h-full"
-                  />
-                </div>
-              )}
-              {etape.images && (
-                <div className="mt-4">
-                  <Carousel
-                    images={etape.images.map((i) => i.responsiveImage!)}
-                    arrowColor={color}
-                  />
-                </div>
-              )}
+          <SimpleBar className="w-full overflow-x-hidden flex">
+            <div className="flex flex-col md:flex-row flex-1">
+              <div className="md:flex-1 max-w-screen-md mb-4 md:mb-0">
+                {etape.video && (
+                  <div className="relative" style={{ paddingTop: '56.25%' }}>
+                    <ReactPlayer
+                      url={etape.video.url!}
+                      controls
+                      width="100%"
+                      height="100%"
+                      className="absolute inset-0 w-full h-full"
+                    />
+                  </div>
+                )}
+                {etape.images && (
+                  <div className="mt-4">
+                    <Carousel
+                      images={etape.images.map((i) => i.responsiveImage!)}
+                      arrowColor={color}
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="md:flex-1 md:ml-12">
+                {etape.sousTitre && (
+                  <h3 className="text-xl md:text-3xl font-plex font-bold mb-4">
+                    {etape.sousTitre}
+                  </h3>
+                )}
+                <p
+                  className="text-xl md:text-3xl font-plex mb-12"
+                  dangerouslySetInnerHTML={{ __html: etape.description! }}
+                />
+                <ul>
+                  {etape.liens.map((lien, i) => {
+                    return (
+                      <li key={i} className="font-plex mb-2 mix-blend-difference text-white">
+                        <a href={lien.url!} target="_blank" rel="noopener noreferrer">
+                          {lien.titre?.length ? lien.titre : lien.url}
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </div>
-            <div className="md:flex-1 md:ml-12">
-              {etape.sousTitre && (
-                <h3 className="text-xl md:text-3xl font-plex font-bold mb-4">{etape.sousTitre}</h3>
-              )}
-              <p
-                className="text-xl md:text-3xl font-plex mb-12"
-                dangerouslySetInnerHTML={{ __html: etape.description! }}
-              />
-              <ul>
-                {etape.liens.map((lien, i) => {
-                  return (
-                    <li key={i} className="font-plex mb-2 mix-blend-difference text-white">
-                      <a href={lien.url!} target="_blank" rel="noopener noreferrer">
-                        {lien.titre?.length ? lien.titre : lien.url}
-                      </a>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
+          </SimpleBar>
         </motion.div>
       )}
     </AnimatePresence>
